5b77a6d59f837c87eddd7430cf30b1f8f01af09c
ALVIN CODER <andelarwanda@Andelas-MacBook-Pro.local>
2023-05-29 09:39:31 +0200

    chore: Setting up tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import "./App.css";
+import { BrowserRouter as Router ,Routes, Route } from "react-router-dom";
+import Home from '../src/pages/LandingPage';
+import ProductPage from "../src/pages/ProductPage";
+
 
 function App() {
   return (
-    <>
-      <div>Hello World!!!!</div>
-    </>
+    <div>
+      <Router>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product-page" element={<ProductPage/>} />
+       
+        </Routes>
+    </Router>
+    </div>
   );
 }
-
 export default App;
