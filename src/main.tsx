a77c618a1a00db7c1bef0e667c1dbecd97fd9e1d
Bryan Murasira <84919967+bryan936M@users.noreply.github.com>
2023-06-01 18:41:28 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// DOM
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
