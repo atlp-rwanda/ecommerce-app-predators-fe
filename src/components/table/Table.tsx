a60611f225034c0579626113d6fd6f0020f3d028
kagaba-etienne <kagabaetienne04@gmail.com>
2023-06-13 14:53:31 +0200

    feature(seller-create-item): enables a seller to create item
    - sets up a product add from
    - notify a seller with appropriate toasts after product creation

--- src/components/table/Table.tsx
+++ src/components/table/Table.tsx
@@ -1,68 +1,127 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react';
 import { fetchProductsCollection } from '../../redux/action/productActions';
 import { useDispatch, useSelector } from 'react-redux';
 import { Product } from '../../redux/reducers/productReducer';
-import DeleteItem from '../vendor/DeleteItem';
+
 export default function Table() {
-  const product = useSelector((state: { CollectionProducts: { data:Product } }) => state.CollectionProducts.data);
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
+  const product = useSelector(
+    (state: { CollectionProducts: { data: Product } }) =>
+      state.CollectionProducts.data
+  );
+  const dispatch = useDispatch();
 
-  const dispatch = useDispatch ()
-   
   useEffect(() => {
+    dispatch(fetchProductsCollection()as any);
+  }, [dispatch]);
 
-     dispatch(fetchProductsCollection()as any)
-	//dispatches an action to the store (redux) 
- 
-  }, [dispatch])
-  console.log('collected product', product?.data) 
+  const handleVisibilityClick = (product: Product) => {
+    setSelectedProduct(product);
+  };
+
+  const handleCloseClick = () => {
+    setSelectedProduct(null);
+  };
+
   return (
-    
-    <div className="  mt-10 mb-6 shadow-lg ">
+    <div className="relative mt-10 mb-6 shadow-lg w-[90%] ">
       <p className="font-medium mb-2 inline-block">Recent Products</p>
-      <div className="w-full pb-2 overflow-x-auto">
+      <div className='flex'>
+      <div className="w-full pb-2 overflow-x-auto flex">
         <table className="font-light w-full table-fixed">
           <thead className="bg-blue-200">
-           
             <tr>
-              <th className="w-32 text-start font-light px-6 py-1">Name</th>
-              <th className="w-32 text-start font-light px-6 py-1">#Id</th>
-              <th className="w-32 text-start font-light px-6 py-1">Products</th>
-              <th className="w-32 text-start font-light px-6 py-1">Category</th>
-              <th className="w-32 text-start font-light px-6 py-1">
+              <th className="w-32 text-start font-light px-6 py-1 border">
+                Name
+              </th>
+              <th className="w-32 text-start font-light px-6 py-1 border">
+                #Id
+              </th>
+              <th className="w-32 text-start font-light px-6 py-1 border">
+                Products
+              </th>
+              <th className="w-32 text-start font-light px-6 py-1 border">
+                Category
+              </th>
+              <th className="w-32 text-start font-light px-6 py-1 border">
                 Description
               </th>
               <th className="w-32 text-start font-light px-6 py-1">Amount</th>
               <th className="w-32 text-start font-light px-6 py-1">Date</th>
               <th className="w-32 text-start font-light px-6 py-1">Action</th>
-            </tr> 
-        
+            </tr>
           </thead>
           <tbody>
-  {product?.data && product.data.map((product: Product) => {
-    return (
-      <tr className="" key={product.id}>
-        <td className="w-32 text-start px-6 py-1">{product.name}</td>
-        <td className="w-32 text-start px-6 py-1">{product.id}</td>
-        <td className="w-32 text-start px-6 py-1">{product.name}</td>
-        <td className="w-32 text-start px-6 py-1">{product.category_id}</td>
-        <td className="w-32 text-start px-6 py-1">{product.description.slice(0,10)}</td>
-        <td className="w-32 text-start px-6 py-1">{product.price}</td>
-        <td className="w-32 text-start px-6 py-1">{product.expiryDate.slice(0,10)}</td>
-        <td className="w-32 text-start px-6 py-1">
-          <div className="symbols flex justify-between">
-            <i className="material-symbols-rounded text-blue-500">visibility</i>
-            <i className="update material-symbols-rounded text-orange-500">edit</i> 
-            <DeleteItem productId={product.id} reason=''/>
-          </div>
-        </td>
-      </tr>
-    );
-  })}
-</tbody>
-
+            {product?.data &&
+              product.data.map((product: Product) => (
+                <tr className="" key={product.id}>
+                  <td className="w-32 text-start px-6 py-1 border">
+                    {product.name.slice(0, 10)}
+                  </td>
+                  <td className="w-32 text-start px-6 py-1 border">
+                    {product.id}
+                  </td>
+                  <td className="w-32 text-start px-6 py-1 border">
+                    {product.name.slice(0, 10)}
+                  </td>
+                  <td className="w-32 text-start px-6 py-1 border">
+                    {product.category_id}
+                  </td>
+                  <td className="w-32 text-start px-6 py-1 border">
+                    {product.description.slice(0, 10)}
+                  </td>
+                  <td className="w-32 text-start px-6 py-1 border">
+                    {product.price}
+                  </td>
+                  <td className="w-32 text-start px-6 py-1 border">
+                    {product.expiryDate.slice(0, 10)}
+                  </td>
+                  <td className="w-32 text-start px-6 py-1 border">
+                    <div className="symbols flex justify-between">
+                      <i
+                        className="material-symbols-rounded text-blue-500 cursor-pointer"
+                        onClick={() => handleVisibilityClick(product)}
+                      >
+                        {' '}
+                        visibility
+                      </i>
+                      <i className="update material-symbols-rounded text-orange-500 ">
+                        edit
+                      </i>
+                      <i className="material-symbols-rounded text-red-500 ">
+                        delete
+                      </i>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
         </table>
       </div>
+
+      {selectedProduct && (
+        <div className="popup p-5 cursor-pointer bg-slate-50">
+          <img
+            src={selectedProduct.picture_urls[0]}
+            alt={selectedProduct.picture_urls[0]}
+          />
+          <div className='p-5'>
+          <h2>Name: {selectedProduct.name}</h2>
+          <p>Description: {selectedProduct.description}</p>
+          <p>Price: {selectedProduct.price}</p>
+          <p>CategoryID: {selectedProduct.category_id}</p>
+          <p>Available Product: {selectedProduct.available}</p>
+          <p>VendorId: {selectedProduct.vendor_id}</p>
+          <p>Instock: {selectedProduct.instock}</p>
+          <p>ExpiryDate: {selectedProduct.expiryDate}</p>
+          <p>CreatedAt: {selectedProduct.createdAt}</p>
+          <p>updatedAt: {selectedProduct.updatedAt}</p>
+          
+          <button className="close-button  bg-[#E2F4FF] my-5 border rounded px-3 " onClick={handleCloseClick}> x</button>
+          </div>
+        </div>
+      )}
+      </div>
     </div>
-    
   );
 }
--- src/dashboard/vendor/VendorPage.tsx
+++ src/dashboard/vendor/VendorPage.tsx
@@ -1,48 +1,74 @@
-import { useState } from 'react'
-import {Table, Product_form } from '../../components';
-import { ToastContainer } from 'react-toastify';
-import HeaderNav from '../../components/HeaderNav';
+import { useState, useEffect } from 'react'
+import { Sidebar, Table, Product_form } from '../../components';
 import cart from "./../../assets/dashboard/cart.svg";
 import add from "./../../assets/dashboard/plus.svg";
-export default function VendorPage() { 
+import profile from "./../../assets/dashboard/account_photo.jpg";
+import { ToastContainer } from 'react-toastify';
+import { fetchProductsCollection } from '../../redux/action/productActions';
+import { useDispatch, useSelector } from 'react-redux';
+
+export default function VendorPage() {
+  const product = useSelector((state: { CollectionProducts: { data: any } }) => state.CollectionProducts.data);
+
+const dispatch = useDispatch ()
+
+useEffect(() => {
+  dispatch(fetchProductsCollection()as any) 	//dispatches an action to the store (redux)
+//console.log('collected product', product?.data)
+}, [dispatch])
+
+
   const [visible, showForm] = useState(false);
   const handleClose = () => showForm(false)
   return (
-    <div className="">
+    <div className="products">
       <ToastContainer />
-      <div className="flex flex-row">       
-        <HeaderNav/>
-        <div className="mt-[5%]  p-5">
-          <div className="cards flex gap-5 flex-wrap justify-between items-end">
-              <div className="pending_orders bg-customBlue text-white rounded-xl w-64 h-24   overflow-hidden p-3">
+      <Sidebar />
+      <div className='md:ml-52'>
+        <div className='pt-20 flex flex-col items-center lg:mx-20'>
+          <div className="dashboard flex flex-col gap-8 flex-wrap w-[90%]">
+            <div className='flex justify-between md:justify-end gap-10 items-center flex-wrap'>
+              <div className="max-[1023px]:flex-1 flex flex-wrap gap-8 justify-between items-end">
+                <div className="dashboard__search shadow-md border rounded-md flex gap-3 p-1">
+                  <i className='material-symbols-rounded text-gray-400'>search</i>
+                  <input  className=" focus:outline-none placeholder:text-gray-400 outline-0 bg-inherit border-0 xs:w-64 md:w-72" type="text" placeholder='Search product ...' />
+                </div>
+                <div className="dashboard__notification flex gap-7 text-blue-500">
+                  <i className="material-symbols-rounded">message</i>
+                  <i className="material-symbols-rounded">notifications</i>
+                </div>
+              </div>
+              <div className=" dashboard__photo hidden lg:block rounded-full overflow-hidden w-20 h-20">
+                <img src={profile} alt="profile_photo" className='object-cover' />
+              </div>
+            </div>
+            <div className="cards flex gap-5 flex-wrap justify-between items-end">
+              <div className="pending_orders bg-customBlue text-white rounded-xl w-64 h-24 relative overflow-hidden p-3">
                 <div className="absolute -bottom-2 -left-3 scale-75">
                   <img src={cart} alt="stock" />
                 </div>
                 <div className="standings flex flex-wrap justify-between h-full w-full">
-                    <h2 className=' text-lg pl-5 justify-self-center'>Instock</h2>
-                    <p className=' text-5xl px-2 self-end'>100</p>
+                  <h2 className=' text-lg pl-5 justify-self-center'>Instock</h2>
+                  <p className=' text-5xl px-2 self-end'>100</p>
                 </div>
               </div>
               <div onClick={() => showForm(true)} className="add_new_product cursor-pointer bg-tertiary h-14 w-14 p-3 flex items-center justify-center rounded-xl">
                 <img src={add} alt="add product" />
               </div>
-          </div>
- 
-          <div className='flex flex-col items-center'> 
-            <Table />
-            <div className="buttons_pagination flex text-sm font-light">
-              <button className='border p-2 text-center rounded-l-lg h-10'>Prev</button>
-              <button className='border-t border-b p-2 text-center h-10 w-10 current bg-slate-500'>1</button>
-              <button className='border p-2 text-center h-10 w-10'>2</button>
-              <p className='border-t border-b p-2 h-10 text-center w-10 inline-block'>...</p>
-              <button className='border-t border-b border-l p-2 text-center h-10 w-10'>40</button>
-              <button className='border p-2 text-center rounded-r-lg h-10'>Next</button>
             </div>
-            <Product_form onClose={handleClose} visible={visible}/>
           </div>
+          <Table />
+          <div className="buttons_pagination flex text-sm font-light">
+            <button className='border p-2 text-center rounded-l-lg h-10'>Prev</button>
+            <button className='border-t border-b p-2 text-center h-10 w-10 current bg-slate-500'>1</button>
+            <button className='border p-2 text-center h-10 w-10'>2</button>
+            <p className='border-t border-b p-2 h-10 text-center w-10 inline-block'>...</p>
+            <button className='border-t border-b border-l p-2 text-center h-10 w-10'>40</button>
+            <button className='border p-2 text-center rounded-r-lg h-10'>Next</button>
+          </div>
+          <Product_form onClose={handleClose} visible={visible}/>
         </div>
       </div>
-
     </div>
   )
 }
