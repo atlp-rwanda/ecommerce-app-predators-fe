23d65e832f771761be2dae58ca7f081ae58912ac
Muhet <muhedarius96@gmail.com>
2023-06-14 22:23:27 +0200

    search product page

--- src/redux/reducers/ProductSlice.tsx
+++ src/redux/reducers/ProductSlice.tsx
@@ -1,52 +1,51 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { fetchProducts,productRemove } from "../action/ProductAction";
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { fetchProducts, productRemove } from '../action/ProductAction';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 const productSlice = createSlice({
-  name: "products",
+  name: 'products',
   initialState: {
     data: [],
-    status: "",
+    status: '',
     loading: false,
-    error: ''
+    error: '',
   },
   reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(fetchProducts.pending, (state) => {
         state.loading = true;
-        state.status = "pending";
+        state.status = 'pending';
         state.error = '';
       })
       .addCase(fetchProducts.fulfilled, (state, action: PayloadAction<any>) => {
         state.loading = false;
-        state.status = "success";
+        state.status = 'success';
         state.data = action.payload;
         state.error = '';
       })
       .addCase(fetchProducts.rejected, (state) => {
         state.loading = false;
-        state.status = "error";
+        state.status = 'error';
       })
       .addCase(productRemove.pending, (state) => {
-        state.loading=true;
-        state.status="pending"
-        state.error=''
-      })
-      .addCase(productRemove.fulfilled, (state,action:PayloadAction<any>) => {
-          state.loading = false;
-          state.status = "success";          
-          toast.success('Item have been deleted successfully'); 
-          state.data = action.payload;
-          state.error = '';
-      })
-       .addCase(productRemove.rejected, (state,action) => {
         state.loading = true;
-        toast.error('Error deleting item');  
-        state.status = "error";  
-        state.error=action.error.message || ' '
-
+        state.status = 'pending';
+        state.error = '';
       })
+      .addCase(productRemove.fulfilled, (state, action: PayloadAction<any>) => {
+        state.loading = false;
+        state.status = 'success';
+        toast.success('Item have been deleted successfully');
+        state.data = action.payload;
+        state.error = '';
+      })
+      .addCase(productRemove.rejected, (state, action) => {
+        state.loading = true;
+        toast.error('Error deleting item');
+        state.status = 'error';
+        state.error = action.error.message || ' ';
+      });
   },
 });
 
